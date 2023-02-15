e264354a349e95f9642ae65e5379462c7fdef52b
WilliamSerrels <2001952@uad.ac.uk>
2023-02-15 17:09:54 +0000

    added test stuff for next bike session

--- CPPWinrt/main.cpp
+++ CPPWinrt/main.cpp
@@ -6,6 +6,17 @@
 #include <future>
 
 #define CYCLE_POWER_MEASURE L"{00002a65-0000-1000-8000-00805f9b34fb}"
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
 
 using namespace winrt;
 using namespace Windows::Foundation;
@@ -52,7 +63,20 @@ int main()
                 if (ConnectDevice(bleDeviceDisplay.DeviceInformation())) { //Sets all vars used in ReadBuffer to target bleDeviceDisplay
                     printf("ConnectDevice Ran Successfully\n");
                     ReadBuffer();   //reads the data in (hopefully)
-                    printf("Data read: %u", readData); //generates annoying compiler error, but should be fine
+                    //printf("Data read: %u", readData.at(0)); //generates annoying compiler error, but should be fine
+                    //printf("Data read: ");
+                    //int i = 0;
+                    //for (uint8_t byte : readData) {
+                    //uint8_t byte = readData.at(0);
+                    //    printf("byte[%i]: %c%c%c%c%c%c%c%c", i, (byte & 0x80 ? '1' : '0'),
+                    //        (byte & 0x40 ? '1' : '0'),
+                    //        (byte & 0x20 ? '1' : '0'),
+                    //        (byte & 0x10 ? '1' : '0'),
+                    //        (byte & 0x08 ? '1' : '0'),
+                    //        (byte & 0x04 ? '1' : '0'),
+                    //        (byte & 0x02 ? '1' : '0'),
+                    //        (byte & 0x01 ? '1' : '0'));
+                    //}
                     printf("\n");
                 }
                 printf("\n");
@@ -209,3 +233,21 @@ void ReadBuffer()
     //return null;
 }
 
+
+//BKCM - Bike communication, the data recieved from the bike
+enum class Tests {
+    DotDataTest,
+    CheckBKCMSize
+};
+
+void testSuite(Tests type, GattReadResult data) {
+    if (type == Tests::DotDataTest) {
+        auto reader = DataReader::FromBuffer(data.Value());
+        reader.ReadBytes(readData);
+        printf("Data read: %u", readData.data());
+    }
+    if (type == Tests::CheckBKCMSize) {
+        printf("Data read: %u", data.Value().Length());
+    }
+
+}
